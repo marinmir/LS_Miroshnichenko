ce97575211ffc326b8c4a8bfb62e7354e38c8135
Miroshnichenko Marina <marinamirosh99@gmail.com>
2020-03-02 02:45:00 +0300

    Initial Commit

---
+++ LS_Miroshnichenko/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LS_Miroshnichenko
+//
+//  Created by Мирошниченко Марина on 02.03.2020.
+//  Copyright © 2020 Мирошниченко Марина. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
